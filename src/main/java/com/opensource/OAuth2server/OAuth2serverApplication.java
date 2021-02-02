707be0ed8f68b28986ec3ed79869f08799359569
aakash <aakash>
2021-02-02 23:18:06 +0530

    Separating Auth server from coupon service

---
+++ src/main/java/com/opensource/OAuth2server/OAuth2serverApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.OAuth2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2serverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensource/OAuth2server/OAuth2serverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.OAuth2server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
